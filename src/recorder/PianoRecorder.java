a529e3d2ef72c301443b4499dde3593c415cf1bb
Fabian Zeiher <fzeiher@gmail.com>
2018-01-14 12:18:50 +0100

    SAVE: playback in background thread (not finished)

--- src/recorder/PianoRecorder.java
+++ src/recorder/PianoRecorder.java
@@ -1,15 +1,20 @@
 package recorder;
 
+import de.ur.mi.graphicsapp.GraphicsApp;
 import piano.PianoKey;
 
 import java.util.ArrayList;
 
-public class PianoRecorder implements Recorder {
+public class PianoRecorder implements Recorder, RecorderPlaybackThreadListener {
+    private RecorderPlaybackThread recorderPlaybackThread;
     private ArrayList<RecorderDataPoint> recording;
     private boolean isRecording = false;
 
     public PianoRecorder() {
         recording = new ArrayList<>();
+        GraphicsApp.println("Init Background Thread");
+        recorderPlaybackThread = new RecorderPlaybackThread(this);
+        GraphicsApp.println("Background Thread Ready");
     }
 
     @Override
@@ -25,15 +30,9 @@ public class PianoRecorder implements Recorder {
 
     @Override
     public void playRecording() {
-        if (!recording.isEmpty() && !isRecording) {
-            for (RecorderDataPoint dataPoint : recording) {
-                try {
-                    Thread.sleep(dataPoint.getWaitTime());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                dataPoint.getKey().playNote(dataPoint.getVelocity());
-            }
+        if (!recording.isEmpty() && !isRecording && !recorderPlaybackThread.isAlive()) {
+            GraphicsApp.println("Opening Background Thread");
+            recorderPlaybackThread.start();
         }
     }
     
@@ -64,6 +63,35 @@ public class PianoRecorder implements Recorder {
 
     private void postprocessing() {
         recording.get(0).setWaitTime(0);
+    }
+
+    @Override
+    public ArrayList<RecorderDataPoint> getRecording() {
+        return recording;
+    }
+
+
+    private class RecorderPlaybackThread extends Thread {
+        private RecorderPlaybackThreadListener threadListener;
+
+        public RecorderPlaybackThread(RecorderPlaybackThreadListener threadListener) {
+            this.threadListener = threadListener;
+        }
+
+        @Override
+        public void run() {
+            GraphicsApp.println("Background Thread Active");
+            ArrayList<RecorderDataPoint> recording = threadListener.getRecording();
+            for (RecorderDataPoint dataPoint : recording) {
+                try {
+                    Thread.sleep(dataPoint.getWaitTime());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                dataPoint.getKey().playNote(dataPoint.getVelocity());
+            }
+            GraphicsApp.println("Closing Background Thread");
+        }
     }
 
 }
---
+++ src/recorder/RecorderPlaybackThreadListener.java
@@ -0,0 +1,9 @@
+package recorder;
+
+import java.util.ArrayList;
+
+public interface RecorderPlaybackThreadListener {
+
+    public ArrayList<RecorderDataPoint> getRecording();
+
+}
